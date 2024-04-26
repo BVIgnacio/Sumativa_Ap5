63e99ed91e8fc79c4ea4d74e431fb074025e75d4
unknown <papiarceus@gmail.com>
2024-04-26 16:25:19 -0500

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAgHNEIq5LK9SV_scKXQm5uxoa-YbXfOjQ",
+  authDomain: "data-base-termometro.firebaseapp.com",
+  projectId: "data-base-termometro",
+  storageBucket: "data-base-termometro.appspot.com",
+  messagingSenderId: "335030946301",
+  appId: "1:335030946301:web:e4494b67f33e5afe63f194"
+  }
 };
 
 /*
